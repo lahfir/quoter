587f19890fef3861e2b2d2832c5e30d751af2b41
Lahfir <nmhlahfir2@gmail.com>
2021-04-18 21:43:09 +0530

    new

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const quotes = db.get("quotes");
 app.use(cors());
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello",
+    });
+});
 
 function isvalidquote(quote) {
     return (
